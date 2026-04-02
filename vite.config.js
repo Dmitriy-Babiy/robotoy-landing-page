fe473d7872da54a5f81510b06aec0fb13c7002bd
Dmitriy Babiy <dimascelecto@gmail.com>
2026-04-02 21:54:42 +0700

    Update project name to 'robotoy-landing-page', add deployment scripts, and enhance Vite configuration for production. Remove unused launch configuration file.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === "production";
+
+  return {
+    base: isProduction ? "/robotoy-landing-page/" : "/",
+    plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  };
+});
