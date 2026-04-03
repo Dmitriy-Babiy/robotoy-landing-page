bc966f1bea340aba84e7a6dcfc2f47db779b6b2a
Dmitriy Babiy <dimascelecto@gmail.com>
2026-04-03 20:38:46 +0700

    deploy on vercel.com

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,7 @@ import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const isProduction = mode === "production";
-
-  return {
-    base: isProduction ? "/robotoy-landing-page/" : "/",
-    plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
-  };
+export default defineConfig({
+  base: "/",
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
 });
